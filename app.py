97233fad5ec61efda29e7029fc46900134ec8d1b
thanks299 <thanksagbeble@gmail.com>
2024-08-23 02:47:52 -0700

    project

--- app.py
+++ app.py
@@ -2,21 +2,21 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index():
     return render_template('index.html')
 
-@app.route('/contact')
+@app.route('/contact', strict_slashes=False)
 def contact():
     return render_template('contact.html')
 
-@app.route('/about')
+@app.route('/about', strict_slashes=False)
 def about():
     return render_template('about.html')
 
-@app.route('/skills')
+@app.route('/skills', strict_slashes=False)
 def skills():
     return render_template('skills.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
