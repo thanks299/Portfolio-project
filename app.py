336ddbcba097ab78092a17e55b0d5619a7d76a45
thanks299 <thanksagbeble@gmail.com>
2024-08-23 01:49:00 -0700

    project

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def index():
+    return render_template('index.html')
 
 @app.route('/contact')
 def contact():
